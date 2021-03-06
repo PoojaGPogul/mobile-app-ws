b27c7cf195341e194462ea770076b35515f9de37
PoojaGPogul <poojapogul@gmail.com>
2021-03-06 20:24:03 +0530

    25. Implementing the Create User Request Model class

--- src/main/java/com/example/app/ws/ui/controller/UserController.java
+++ src/main/java/com/example/app/ws/ui/controller/UserController.java
@@ -4,18 +4,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.app.ws.ui.model.request.UserDetailsRequestModel;
+
 @RestController
-@RequestMapping("users")
+@RequestMapping("users") // http://localhost:8080/users
 public class UserController {
 	@GetMapping
 	public String getUser() {
 		return "Get user was called";
 	}
 	@PostMapping
-	public String createUser() {
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		return "Create user was called";
 	}
 	@PutMapping
