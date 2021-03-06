3ba77ef2d2096093e69733a99459a570bac4a140
PoojaGPogul <poojapogul@gmail.com>
2021-03-06 21:43:13 +0530

    30. Implementing UserEntity class

---
+++ src/main/java/com/example/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+ package com.example.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.app.ws.service.UserService;
+import com.example.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/com/example/app/ws/ui/controller/UserController.java
+++ src/main/java/com/example/app/ws/ui/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.app.ws.ui.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +10,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.app.ws.service.UserService;
+import com.example.app.ws.shared.dto.UserDto;
 import com.example.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.example.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
 	@GetMapping
 	public String getUser() {
 		return "Get user was called";
 	}
 	@PostMapping
 	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
-		return null;
+		
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails , userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		return returnValue;
 	}
 	@PutMapping
 	public String updateUser() {
