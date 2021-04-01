65b4476ab6f789863d2e8f718b1ce82d74ae7537
PoojaGPogul <poojapogul@gmail.com>
2021-04-01 22:11:48 +0530

    67. Trying the Update User Details API Call

--- src/main/java/com/example/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/app/ws/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.app.ws.service.impl;
 
+import java.beans.Beans;
 import java.util.ArrayList;
 
 import org.springframework.beans.BeanUtils;
@@ -10,11 +11,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.app.ws.exceptions.UserServiceException;
 import com.example.app.ws.io.entity.UserEntity;
 import com.example.app.ws.io.repositories.UserRepository;
 import com.example.app.ws.service.UserService;
 import com.example.app.ws.shared.Utils;
 import com.example.app.ws.shared.dto.UserDto;
+import com.example.app.ws.ui.model.response.ErrorMessage;
+import com.example.app.ws.ui.model.response.ErrorMessages;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -85,6 +89,25 @@ public class UserServiceImpl implements UserService {
 		}
 		BeanUtils.copyProperties(userEntity, returnValue);
 
+		return returnValue;
+	}
+
+	@Override
+	public UserDto updateUser(String userId, UserDto userDto) {
+		UserDto returnValue = new UserDto();
+		
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		if (userEntity == null) {
+			throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+		}
+		
+		userEntity.setFirstName(userDto.getFirstName());
+		userEntity.setLastName(userDto.getLastName());
+		
+		UserEntity updatedUserDetails = userRepository.save(userEntity);
+		
+		BeanUtils.copyProperties(updatedUserDetails, returnValue);
+		
 		return returnValue;
 	}
 
--- src/main/java/com/example/app/ws/ui/controller/UserController.java
+++ src/main/java/com/example/app/ws/ui/controller/UserController.java
@@ -54,9 +54,27 @@ public class UserController {
 		
 		return returnValue;
 	}
-	@PutMapping
-	public String updateUser() {
-		return "Update user was called";
+	
+	@PutMapping(path="/{id}",
+			consumes ={ MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces= { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+			)
+	public UserRest updateUser(@PathVariable String id, @RequestBody UserDetailsRequestModel userDetails) {
+
+		UserRest returnValue = new UserRest();
+		
+		if(userDetails.getFirstName().isEmpty()) {
+			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
+		}
+		
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails , userDto);
+		
+		UserDto updatedUser = userService.updateUser(id, userDto);
+		BeanUtils.copyProperties(updatedUser, returnValue);
+		
+		return returnValue;
+		
 	}
 	@DeleteMapping
 	public String deleteUser() {
