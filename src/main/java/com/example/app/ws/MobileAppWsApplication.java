e892090fc7baecfcc4c5d1a3c620ded2cd7c0d23
PoojaGPogul <poojapogul@gmail.com>
2021-04-04 14:31:12 +0530

    96. Creating AddressDTO

--- src/main/java/com/example/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/example/app/ws/MobileAppWsApplication.java
@@ -2,11 +2,13 @@ package com.example.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
@@ -19,5 +21,9 @@ public class MobileAppWsApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsApplication.class);
+	}
 }
