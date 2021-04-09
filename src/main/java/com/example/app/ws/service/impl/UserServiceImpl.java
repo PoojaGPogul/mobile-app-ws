78af145c567b9871f6b7953cdc052c01a56f4c7c
PoojaGPogul <poojapogul@gmail.com>
2021-04-09 21:38:33 +0530

    139. Prevent Users with UnverifiedEmail Address to Login

--- src/main/java/com/example/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/app/ws/service/impl/UserServiceImpl.java
@@ -57,6 +57,7 @@ public class UserServiceImpl implements UserService {
 		userEntity.setUserId(publicUserId);
 		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
 		userEntity.setEmailVerificationToken(utils.generateEmailVerificationToken(publicUserId));
+		userEntity.setEmailVerificationStatus(false);
 		
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 
@@ -71,9 +72,13 @@ public class UserServiceImpl implements UserService {
 		if (userEntity == null) {
 			throw new UsernameNotFoundException(email);
 		}
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), 
+				userEntity.getEmailVerificationStatus(), true, true, true, new ArrayList<>());
+		
+		//return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 
-		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
-
+		
 	}
 
 	@Override
