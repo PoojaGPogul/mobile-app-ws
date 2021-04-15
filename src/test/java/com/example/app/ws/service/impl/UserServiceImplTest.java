5deb001593c5426c5c53e48502c0ac1af25847c3
PoojaGPogul <poojapogul@gmail.com>
2021-04-15 22:21:11 +0530

    175. Testing the createUser() Service Method

--- src/test/java/com/example/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/app/ws/service/impl/UserServiceImplTest.java
@@ -2,7 +2,13 @@ package com.example.app.ws.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,10 +16,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.app.ws.io.entity.UserEntity;
 import com.example.app.ws.io.repositories.UserRepository;
+import com.example.app.ws.shared.Utils;
+import com.example.app.ws.shared.dto.AddressDto;
 import com.example.app.ws.shared.dto.UserDto;
+
 
 class UserServiceImplTest {
 
@@ -22,20 +32,35 @@ class UserServiceImplTest {
 
 	@Mock
 	UserRepository userRepository;
+	
+	@Mock
+	Utils utils;
 
+	@Mock
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
+	
+	String userId = "qwerty";
+	String encodedPassword = "kdfnwkerjskdfn";
+	UserEntity userEntity;
+ 
 	@BeforeEach
 	void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
+		
+		userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setFirstName("Pooja");
+		userEntity.setUserId(userId);
+		userEntity.setEncryptedPassword(encodedPassword);
+		userEntity.setEmail("test@test.com");
+		userEntity.setEmailVerificationToken("sdfsdfsdfsdfdsf");
+		
 	}
 
 	@Test
 	void testGetUser() {
-
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setFirstName("Pooja");
-		userEntity.setUserId("qwerty");
-		userEntity.setEncryptedPassword("kdfnwkerjskdfn");
 
 		// Mocking - providing fake result
 		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
@@ -57,12 +82,37 @@ class UserServiceImplTest {
 
 		// Assertions
 		assertThrows(UsernameNotFoundException.class, 
-		
 				() -> {
 					userService.getUser("test@test.com");
 				}
 		);
-
+	}
+	
+	@Test
+	final void testCreateUser() {
+		
+		when(userRepository.findByEmail(anyString())).thenReturn(null);
+		when(utils.generateAddressId(anyInt())).thenReturn("asdsdfsdfsdfdsf");
+		when(utils.generateUserId(anyInt())).thenReturn(userId);
+		when(bCryptPasswordEncoder.encode(anyString())).thenReturn(encodedPassword);
+		when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+		
+		AddressDto addressDto = new AddressDto();
+		addressDto.setType("shipping");
+		
+		List<AddressDto> addresses = new ArrayList<>();
+		addresses.add(addressDto);
+		
+		UserDto userDto = new UserDto();
+		userDto.setAddresses(addresses);
+		
+		UserDto storedUserDetails = userService.createUser(userDto);
+		
+		assertNotNull(storedUserDetails);
+		assertEquals(userEntity.getFirstName(), storedUserDetails.getFirstName());
+		
+		
+		
 	}
 
 }
