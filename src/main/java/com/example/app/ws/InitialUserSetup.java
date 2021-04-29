ed2481f7cfc2ec0add6a3454f89d83c0b1dd38fa
PoojaGPogul <poojapogul@gmail.com>
2021-04-29 20:11:36 +0530

    256. Create User with Admin Role

--- src/main/java/com/example/app/ws/InitialUserSetup.java
+++ src/main/java/com/example/app/ws/InitialUserSetup.java
@@ -1,12 +1,23 @@
 package com.example.app.ws;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.example.app.ws.io.entity.AuthorityEntity;
+import com.example.app.ws.io.entity.RoleEntity;
+import com.example.app.ws.io.entity.UserEntity;
 import com.example.app.ws.io.repositories.AuthorityRepository;
+import com.example.app.ws.io.repositories.RoleRepository;
+import com.example.app.ws.io.repositories.UserRepository;
+import com.example.app.ws.shared.Utils;
 
 @Component
 public class InitialUserSetup {
@@ -14,7 +25,20 @@ public class InitialUserSetup {
 	@Autowired
 	AuthorityRepository authorityRepository;
 	
+	@Autowired
+	RoleRepository roleRepository;
+	
+	@Autowired
+	Utils utils;
+	
+	@Autowired 
+	BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	UserRepository userRepository;
+	
 	@EventListener
+	@Transactional
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		System.out.println("From Application ready event...");
 		
@@ -22,8 +46,26 @@ public class InitialUserSetup {
 		AuthorityEntity writeAuthority = createAuthority("WRITE_AUTHORITY");
 		AuthorityEntity deleteAuthority = createAuthority("DELETE_AUTHORITY");
 		
+		RoleEntity userRole = createRole("ROLE_USER", Arrays.asList(readAuthority, writeAuthority));
+		RoleEntity adminRole = createRole("ROLE_ADMIN", Arrays.asList(readAuthority, writeAuthority, deleteAuthority));
+		
+		if(adminRole == null) {
+			return;
+		}
+		UserEntity adminUser = new UserEntity();
+		adminUser.setFirstName("Pooja");
+		adminUser.setLastName("Pogul");
+		adminUser.setEmail("test@test.com");
+		adminUser.setEmailVerificationStatus(true);
+		adminUser.setUserId(utils.generateUserId(30));
+		adminUser.setEncryptedPassword(bCryptPasswordEncoder.encode("12345678"));
+		adminUser.setRoles(Arrays.asList(adminRole));
+		
+		userRepository.save(adminUser);
+		
 	}
 	
+	@Transactional
 	private AuthorityEntity createAuthority(String name) {
 		
 		AuthorityEntity authorityEntity = authorityRepository.findByName(name);
@@ -33,6 +75,20 @@ public class InitialUserSetup {
 			authorityRepository.save(authorityEntity);
 		}
 		return authorityEntity;
+		
+	}
+
+	@Transactional
+	private RoleEntity createRole(String name, Collection<AuthorityEntity> authorities) {
+		
+		RoleEntity roleEntity = roleRepository.findByName(name);
+		
+		if(roleEntity == null) {
+			roleEntity = new RoleEntity(name);
+			roleEntity.setAuthorities(authorities);
+			roleRepository.save(roleEntity);
+		}
+		return roleEntity;
 		
 	}
 	
